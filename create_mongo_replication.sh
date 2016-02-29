40b6018b98d22ea7c4d9887210fca60f6f487c32
Lior Kuperiu <liorkuperiu@Liors-MacBook-Pro.local>
2016-02-29 23:05:05 +0200

    v1.1

--- create_mongo_replication.sh
+++ create_mongo_replication.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+<<COMMENT
+Automatically install mongodb 3.2 and configure 3 nodes replication
+Instruction:
+1. Pick Amazon Linux AMI as your instance
+2. Configure 3 instances for luncg
+3. Copy paste this script to the userdata field
+4. Make sure that all traffic is allowd in the private subnet of instances
+COMMENT
 declare -a arr
-sudo yum install -y fping.x86_64
 install_mongo () {
   #!/bin/bash
   sudo echo "[mongodb-org-3.2]" >> /etc/yum.repos.d/mongodb-org-3.2.repo
@@ -8,26 +15,16 @@ install_mongo () {
   sudo echo "baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.2/x86_64/" >> /etc/yum.repos.d/mongodb-org-3.2.repo
   sudo echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb-org-3.2.repo
   sudo echo "enabled=1" >> /etc/yum.repos.d/mongodb-org-3.2.repo
-
-  lunch_index=$(curl -s http://169.254.169.254/latest/meta-data/ami-launch-index/)
   sudo yum install -y mongodb-org
   sudo sed -i 's/bindIp/#bindIp/g' /etc/mongod.conf
   sudo echo "replication:" >> /etc/mongod.conf
   sudo echo "  replSetName: rs0" >> /etc/mongod.conf
   sudo /etc/init.d/mongod start
-
-  if [ 0 -eq $lunch_index ]; then
-    #activate mongo
-    is_on=$(nc -z localhost 27017)
-    while [ -z "$is_on" ]; do
-     sleep 10
-     is_on=$(nc -z localhost 27017)
-    done
-    sleep 10
-    mongo --eval "rs.initiate()"
-  fi
+  chkconfig mongod on
 }
+#Scans the subnet for the 2 secondaries nodes
 discover () {
+  sudo yum install -y fping.x86_64
   nodes=0
   begin=$(route -n | tail -n1 | awk {'print $1'})
   end=$(ifconfig | head -n2 | tail -n1 | awk {'print $3'} | sed 's/Bcast://g')
@@ -56,17 +53,18 @@ discover () {
 }
 create_repication () {
   install_mongo
-  az=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-  az=$(echo $az | sed 's/.$//g')
-  discover
   lunch_index=$(curl -s http://169.254.169.254/latest/meta-data/ami-launch-index/)
   if [ 0 -eq $lunch_index ]; then
-    var=$(echo ${arr[0]} | sed 's/\./-/g')
-    var=$(echo "ip-"$var"."$az".compute.internal")
-    mongo --eval "rs.add('$var')"
-    var=$(echo ${arr[1]} | sed 's/\./-/g')
-    var=$(echo "ip-"$var"."$az".compute.internal")
-    mongo --eval "rs.add('$var')"
+    az=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    az=$(echo $az | sed 's/.$//g')
+    discover
+    mongo --eval "rs.initiate()"
+    dns=$(echo ${arr[0]} | sed 's/\./-/g')
+    dns=$(echo "ip-"$dns"."$az".compute.internal")
+    mongo --eval "rs.add('$dns')"
+    dns=$(echo ${arr[1]} | sed 's/\./-/g')
+    dns=$(echo "ip-"$dns"."$az".compute.internal")
+    mongo --eval "rs.add('$dns')"
   fi
 }
 create_repication
