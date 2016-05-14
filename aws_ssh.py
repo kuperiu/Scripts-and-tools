02ad302bed9b8967c2e991d43194bc979a9113a3
Lior Kuperiu <lior.kuperiu@LiorK.local>
2016-05-14 23:31:22 +0300

    v1.0

--- aws_ssh.py
+++ aws_ssh.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import boto3,optparse,subprocess,sys
+import boto3,argparse,subprocess,sys
 
 '''
 connects to an ec2 instance by a tag name
@@ -14,19 +14,21 @@ def get_public_ip(tag):
     for instance in instances:
         return instance.public_ip_address
     print("Tag name was not found")
+    sys.exit(1)
 
 
 def main():
-  p = optparse.OptionParser(description='Connect to an EC2 instance by a tag name')
-  p.add_option('-t', '--tag', action ='store', help='Tag name')
-  p.add_option('-i', '--certificate', action ='store', help='Certificate path')
-  options, arguments = p.parse_args()
-  if options.tag and options.certificate:
-      ip = get_public_ip(options.tag)
-      cmd = 'ssh ubuntu@' + ip + ' -i ' + options.certificate
-      subprocess.call(cmd,shell=True)
-  else:
-      print('Missing arguments')
-
+    parser = argparse.ArgumentParser(description="Connect to an EC2 instance by a tag name")
+    parser.add_argument("-t", "--tag", action="store", help="The tag name of the EC2 instance ")
+    parser.add_argument("-i", "--certificate", action="store",help="The key location")
+    args = parser.parse_args()
+    if args.tag and args.certificate:
+        ip = get_public_ip(args.tag)
+        cmd = 'ssh ubuntu@' + ip + ' -i ' + args.certificate
+        subprocess.call(cmd,shell=True)
+    elif not args.tag:
+        print("Tag name is missing")
+    else:
+        print("Certificate location is missing")
 if __name__ == '__main__':
   main()
