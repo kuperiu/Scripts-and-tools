2310f82fbf3613b2d34895eac2cf0f9236bb7abd
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-09-12 19:55:04 +0200

    top_uri bash stdin example

--- top_uris.sh
+++ top_uris.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+<<COMMENT
+top uri implementation in bash
+to test stdin do:
+echo apache.log | ./top_uris.sh
+COMMENT
+
 lines=()
 while read line; do
     lines+=($(echo $line | awk '{print $7}'))
 done < $(cat /dev/stdin)
-
 
 printf '%s\n' "${lines[@]}" | sort | uniq -c | sort -r | head -n4
