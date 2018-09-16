a2245a1cedaad34ec0e0b1b9637e750915964c9b
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-09-16 19:13:51 +0200

    with colors

--- top_uris.sh
+++ top_uris.sh
@@ -12,3 +12,10 @@ while read line; do
 done < $(cat /dev/stdin)
 
 printf '%s\n' "${lines[@]}" | sort | uniq -c | sort -r | head -n$1
+
+####with colors
+#cat apache.log | awk '{print $7}' | sort | uniq -c | sort -r | head | awk '{print "\033[32m"$1 " "  "\033[36m"$2}'
+# 30 - black   34 - blue          
+#    31 - red     35 - magenta       
+#    32 - green   36 - cyan          
+#    33 - yellow  37 - white    
